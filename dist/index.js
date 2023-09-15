ec046c8935bca14ec05fd83235ac70d3d4d0aea5
Микола <hulkovskij@gmail.com>
2023-09-15 10:54:15 +0300

    /

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,15 @@ if (button && input) {
         concatenation(input.value, "hello!");
     });
 }
+var Role;
+(function (Role) {
+    Role[(Role["ADMIN"] = 0)] = "ADMIN";
+    Role[(Role["USER"] = 1)] = "USER";
+})(Role || (Role = {}));
+var person = {
+    role: Role.ADMIN,
+};
+if (person.role === Role.ADMIN) {
+    console.log("Role: ", Role.ADMIN);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,15 @@ if (button && input) {
     concatenation(input.value, "hello!");
   });
 }
+
+var Role;
+(function (Role) {
+  Role[(Role["ADMIN"] = 0)] = "ADMIN";
+  Role[(Role["USER"] = 1)] = "USER";
+})(Role || (Role = {}));
+var person = {
+  role: Role.ADMIN,
+};
+if (person.role === Role.ADMIN) {
+  console.log("Role: ", Role.ADMIN);
+}
